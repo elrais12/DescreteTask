048e8c87cb552fc7975a273d67da1e456e74eef1
elrais12 <121350913+elrais12@users.noreply.github.com>
2022-12-24 17:49:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿ // reading the ism num 
+Console.WriteLine("enter the in ");
+int s = int .Parse(Console.ReadLine());
+// reading the 2nd num 
+Console.WriteLine(" enter the 2n ");
+int e = int .  Parse(Console.ReadLine());
+int sum = 0;
+Console.WriteLine(" the prime numbers from {0} to {1} is",s,e);
+for (int i = s; i <= e; i++)
+{
+    for ( int x = 1; x<= i; x++)
+    {
+        if (i % x == 0)
+            sum++; 
+    }
+    if ( sum== 2)
+    {
+        Console.WriteLine(i);
+        sum = 0; 
+
+    }
+    else
+    {
+        sum = 0; 
+    }
+    
+}
